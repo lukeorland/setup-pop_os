779a046401a86693485e53ba1596914375f8460f
Luke Orland <orluke@gmail.com>
2020-09-01 00:31:24 -0400

    Add my personal configs

--- scripts/3-run_ansible.sh
+++ scripts/3-run_ansible.sh
@@ -5,7 +5,7 @@ venv_dir="$HOME/.virtualenvs/ansible/bin"
 
 run_playbook() {
     source "$venv_dir/activate"
-    eval "ansible-galaxy install --roles-path $main_dir/roles jaredhocutt.gnome_extensions"
+    eval "ansible-galaxy install -r requirements.yml"
     eval "ansible-playbook $main_dir/playbook.yml -e ansible_python_interpreter=$venv_dir/python --ask-become-pass"
 }
 
