b54498220b144a05f3637697fe6a8b226353b5a8
Luke Orland <orluke@gmail.com>
2020-09-03 10:53:29 -0400

    verbose for run_role, a little more idempotence

--- scripts/3a-run_ansible_role.sh
+++ scripts/3a-run_ansible_role.sh
@@ -6,7 +6,7 @@ tagname=$1
 
 run_role() {
     source "$venv_dir/activate"
-    eval "ansible-playbook $main_dir/playbook.yml -e ansible_python_interpreter=$venv_dir/python --tag '$tagname' --ask-become-pass"
+    eval "ansible-playbook -vv $main_dir/playbook.yml -e ansible_python_interpreter=$venv_dir/python --tag '$tagname' --ask-become-pass"
 }
 
 echo "running role..."
